7dc8ad26ff7f3b7f435d650ea134573d9063f68b
Ullas Acharya <Ullas.22cs174@sode-edu.in>
2026-03-04 19:51:16 +0530

    feat:Connected to mongoDB and linked the navbar files

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -19,9 +19,9 @@ export function Navbar() {
   }, [])
 
   const navLinks = [
-    { href: "#features", label: "Features" },
-    { href: "#how-it-works", label: "How It Works" },
-    { href: "#use-cases", label: "Use Cases" },
+    { href: "/#features", label: "Features" },
+    { href: "/#how-it-works", label: "How It Works" },
+    { href: "/#use-cases", label: "Use Cases" },
   ]
 
   return (
